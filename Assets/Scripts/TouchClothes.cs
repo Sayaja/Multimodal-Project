147b4666aa4af4d7712aa465b2b86db6e511b42f
Sayaja <35453348+Sayaja@users.noreply.github.com>
2018-12-11 18:50:14 +0100

    Modell placeras bara 1 gång + registrerar klick på modellen

---
+++ Assets/Scripts/TouchClothes.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchClothes : MonoBehaviour {
+
+public GameObject testObj;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// Check for click on screen
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began){
+
+			// Checks if user clicked on AR modell
+			Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+			RaycastHit rayhit = new RaycastHit();
+			if (Physics.Raycast(ray, out rayhit)) {
+				// GameObject hitThing = rayhit.collider.gameObject;
+
+				//Fetch the Renderer from the GameObject
+				Renderer rend = testObj.GetComponent<Renderer>();
+
+				//Create a new Material
+				Material material = new Material(Shader.Find("Standard"));
+				material.color = Color.green;
+
+				//Switch to new material
+				rend.material = material;
+			}
+
+		}
+
+	}
+}
